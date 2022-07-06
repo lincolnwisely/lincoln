a528d276c05563b6f94900ae97ffe1cc9ed4ba35
Lincoln Wisely <lwisely@hlkagency.com>
2022-07-06 13:21:44 -0500

    Hardcode title/meta

--- src/components/head.js
+++ src/components/head.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { Helmet } from "react-helmet"
 
-export default function Head({ title, description, image }) {
+export default function Head({ title = "Lincoln Wisely | Le Website", description="*in a cool robot voice* welcome, earthlings", image }) {
   return (
     <Helmet
       htmlAttributes={{
@@ -9,14 +9,13 @@ export default function Head({ title, description, image }) {
       }}
     >
       <meta charSet="utf-8" />
-      <title>{title}</title>
-      {description && (
+      <title>Lincoln Wisely | Le Website</title>
         <meta
           name="description"
           property="og:description"
-          content={description}
+          content="*in a cool robot voice* welcome, earthlings"
         />
-      )}
+
       <meta property="og:title" content={title} />
       {image && <meta property="og:image" content={image.url} />}
       <meta name="twitter:card" content="summary" />
